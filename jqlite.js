dabd9ccd16d889790758c9c2c616490c2dfcd6c3
tianzx <436011080@qq.com>
2016-04-07 22:02:53 +0800

    Initial commit

---
+++ jqlite.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tianzx on 2016/4/7.
+ */
+(function(){
+
+    console.log("tianzx");
+    //window.jqlite = window.$ = jqlite;
+    jqlite = null;
+    return {
+        jqlite : jqlite
+    }
+})();
